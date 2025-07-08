2a12f8f77272d51485689326c304f8e6e43b4b2f
NHHieuu <hieuhong08122003@gmail.com>
2025-07-08 15:03:43 +0700

    thưc hanh slide 67

---
+++ MvcProject/Migrations/20250708075436_ThemEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Person",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Person");
+        }
+    }
+}
--- MvcProject/Models/Person.cs
+++ MvcProject/Models/Person.cs
@@ -8,4 +8,5 @@ public class Person
     public required string PersonId { get; set; }
     public required string FullName { get; set; }
     public string? Address { get; set; }
+     public string? Email { get; set; }
 }
